3e4d1c2cdda8ca268d2d4c959644c3573960bd53
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-11-29 01:17:25 +0600

    add

--- First code/test.py
+++ First code/test.py
@@ -3,3 +3,16 @@ a = 10
 b = 4
 sum = a + b
 print("Sum is: ", sum)
+
+# n = input()
+# print(n)
+
+if sum %2 == 0:
+    print("This is even number")
+else:
+    print("This is odd number")
+
+for i in range(1, 6):
+    print(i)
+
+    
