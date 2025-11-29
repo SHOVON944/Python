05ebb62a23c629ac93ce6ebe2330162286e01122
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-11-29 23:13:09 +0600

    a

---
+++ First code/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4]
+# for n in numbers:
+#     print(n)
+
+
+# # function
+# def add(a, b):
+#     return a + b
+
+# print(add(3, 5))
--- First code/test.py
+++ First code/test.py
@@ -1,33 +1,47 @@
-print("Hello, World!!!")
-a = 10
-b = 4
-sum = a + b
-print("Sum is: ", sum)
+# print("Hello, World!!!")
+# a = 10
+# b = 4
+# sum = a + b
+# print("Sum is: ", sum)
 
-# n = input()
-# print(n)
+# # n = input()
+# # print(n)
 
-if sum %2 == 0:
-    print("This is even number")
-else:
-    print("This is odd number")
+# if sum %2 == 0:
+#     print("This is even number")
+# else:
+#     print("This is odd number")
 
-for i in range(1, 6):
-    print(i)
+# for i in range(1, 6):
+#     print(i)
 
-i = 1
-while i<4:
-    print(i)
-    i += 1
+# i = 1
+# while i<4:
+#     print(i)
+#     i += 1
 
 # List
-numbers = [1, 2, 3, 4]
-for n in numbers:
-    print(n)
+# numbers = [1, 2, 3, 4]
+# for n in numbers:
+#     print(n)
 
 
-# function
-def add(a, b):
-    return a + b
+# # function
+# def add(a, b):
+#     return a + b
 
-print(add(3, 5))
+# print(add(3, 5))
+
+# Dictionary
+# student = {
+#     "name" : "Shovon",
+#     "ID" : "2302056",
+#     "Faculty" : "CSE",
+# }
+# print(student["name"])
+# print(student["Faculty"])
+
+
+# List Comprehsion
+square = [x*x*x for x in range(1, 6)]
+print(square)
