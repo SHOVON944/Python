d25687ae0e6f6301d678991d68e3c6040db071f2
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2026-01-31 23:51:43 +0600

    add

--- Apna College/start.py
+++ Apna College/start.py
@@ -2,3 +2,28 @@ name = "SHOVON"
 print(type(name))
 
 print(type(2.3))
+
+name1 = "Sk"
+name2 = 'SK'
+name3 = "'Sk'"
+print(name1)
+print(name2)
+print(name3)
+print(type(True))
+a = None
+print(type(a))
+
+
+
+a, b = 2, 3
+print(a)
+print(b)
+
+
+txt = "@"
+print(2*txt*2)
+
+
+A, B = "2", 3
+Txt = "@"
+print((A+Txt)*B)
