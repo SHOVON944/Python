7d77e7bf94d8a832cf8cb6142acbb4756f67dd0f
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2026-02-11 20:54:13 +0600

    add

---
+++ Apna College/loop/for/basic.py
@@ -0,0 +1,8 @@
+nums = [1, 2, 3, 4, 5]
+
+for val in nums:
+    print(val)
+
+str = "Clash"
+for ch in str:
+    print(ch)
---
+++ Apna College/loop/for/for_else loop(s).py
@@ -0,0 +1,6 @@
+nums = [1, 2, 3, 4, 5]
+
+for val in nums:
+    print(val)
+else:
+    print("END")
